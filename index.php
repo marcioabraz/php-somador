33c8b5d9ef3b78bf4982ced83881703f87f8d618
= <=>
2022-01-12 17:17:00 -0300

    somador

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Somador</title>
+</head>
+<body>
+<Form name="f1" action="soma.php" method="Get">
+    <input type="text" name="valor1" size="10" maxlength="10"><br />
+    <input type="text" name="valor2" size="10" maxlength="10"><br />
+    <input type="submit" value="Calcular">
+    <input type="submit" value="Limpar">
+</form>
+</body>
+</html>
---
+++ soma.php
@@ -0,0 +1,6 @@
+<?php
+$v1=$_GET['valor1'];
+$v2=$_GET['valor2'];
+$total=$v1+$v2;
+echo"A soma é:$total";
+?>
